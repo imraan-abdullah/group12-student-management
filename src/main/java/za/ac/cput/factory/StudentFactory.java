fcd7f32c8f8d76525e2422c78025e3a15b2d6789
Ameer <220005060@mycput.ac.za>
2022-06-16 18:10:49 +0200

    Q1, Q2 & Q3 - Ameer Samsodien 220005060

---
+++ src/main/java/za/ac/cput/factory/StudentFactory.java
@@ -0,0 +1,23 @@
+//Ameer Samsodien
+//220005060
+//StudentFactory.java
+//12 June
+
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Name;
+import za.ac.cput.domain.Student;
+import za.ac.cput.helper.StringHelper;
+
+public class StudentFactory {
+    public static Student build(String studentId, String email, Name name){
+        StringHelper.checkStringParam("studentId", studentId);
+        StringHelper.checkStringParam("email", email);
+        //StringHelper.checkStringParam("name", name);
+        StringHelper.checkId(studentId);
+        return new Student.Builder().setStudentId(studentId)
+                .setEmail(email)
+                .setName(name)
+                .build();
+    }
+}
